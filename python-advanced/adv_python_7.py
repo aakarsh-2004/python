cd83dd6ad349533ceacdd71d82b512ca502944d1
Aakarsh Beohar <aakarshbeohar2004@gmail.com>
2023-08-26 09:53:00 +0530

    Worked with packages

---
+++ python-advanced/adv_python_7.py
@@ -0,0 +1,5 @@
+import modules.module
+# from modules.module import *
+
+print(modules.module.cart("Apple"))
+
---
+++ python-advanced/modules/module.py
@@ -0,0 +1,4 @@
+def cart(item):
+    my_cart = []
+    my_cart.append(item)
+    return my_cart
